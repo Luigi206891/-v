6edced952c0a435670a1ddff95596f09ac866a9e
Luigi206891 <101366677+Luigi206891@users.noreply.github.com>
2022-04-12 08:45:31 -0500

    R
    R

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -346,16 +346,28 @@ class Alphabet extends FlxSpriteGroup
 
 	override function update(elapsed:Float)
 	{
-		if (isMenuItemCenter)
+		if (isMenuItem)
 		{
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
 			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
 			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
 			if(forceX != Math.NEGATIVE_INFINITY) {
-				 screenCenter(x);
+				x = forceX;
 			} else {
-				 screenCenter(x);
+				x = FlxMath.lerp(x, (targetY * 20) + 90 + xAdd, lerpVal);
+			}
+		}
+				if (isMenuItemCenter)
+		{
+			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
+
+			var lerpVal:Float = CoolUtil.boundTo(elapsed * 9.6, 0, 1);
+			y = FlxMath.lerp(y, (scaledY * yMult) + (FlxG.height * 0.48) + yAdd, lerpVal);
+			if(forceX != Math.NEGATIVE_INFINITY) {
+				screenCenter(x);
+			} else {
+				screenCenter(x);
 			}
 		}
 
